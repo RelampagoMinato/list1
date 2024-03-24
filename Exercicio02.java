9a630a43e1037ab4185a26404cd354486b61baaa
RelampagoMinato <julioandreguedes@gmail.com>
2024-03-24 14:37:57 -0300

    Exercicio 2

---
+++ Exercicio02.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+    public static void executar(){
+
+    
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Digite o primeiro número: ");
+        double num1 = leitor.nextDouble();
+        System.out.println("Digite o segundo número: ");
+        double num2 = leitor.nextDouble();
+        double  soma = num1 + num2;
+
+
+        System.out.println("A soma é:" + soma );
+    
+    
+    leitor.close();
+    }
+  
+}
